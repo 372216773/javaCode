2462e64a9c826c052c68090a6c72e1c5a021afa3
inviting <53403310+372216773@users.noreply.github.com>
2021-01-06 17:48:25 +0800

    Java Code

--- inheritance/src/com/wj/Demo.java
+++ inheritance/src/com/wj/Demo.java
@@ -1,20 +1,52 @@
 package com.wj;
 
-class Animal {
-    private String name;
-    public void eat() {
-        System.out.println();
+import java.util.Arrays;
+
+//自定义类型排序,继承Comparable<自定义类型>接口
+//并且实现这个接口的抽象方法
+class Student implements Comparable<Student>{
+    public String name;
+    public int age;
+    public double score;
+
+    public Student(String name,int age,double score) {
+        this.name=name;
+        this.age=age;
+        this.score=score;
     }
-}
-class Cat {
 
-}
-class Bird {
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", score=" + score +
+                '}';
+    }
 
+    @Override//从小到大排序
+    public int compareTo(Student o) {
+        if (this.age>o.age) {
+            return -1;
+        }else if (this.age==o.age) {
+            return 0;
+        }else {
+            return 1;
+        }
+    }
 }
 public class Demo {
     public static void main(String[] args) {
+        //自定义类型数组,元素为student对象
+        Student[] student=new Student[3];
+        student[0] =new Student("A",162,4.65);
+        student[1] =new Student("B",142,45.2);
+        student[2] =new Student("C",172,4.5);
+        System.out.println(Arrays.toString(student));
+        //调用sort排序,自定义类型的话,不知道以什么来进行排序,
+        // 所以就要继承Comparable接口,重写compareTo方法,规定以什么来排序
+        Arrays.sort(student);
 
-
+        System.out.println(Arrays.toString(student));
     }
 }
