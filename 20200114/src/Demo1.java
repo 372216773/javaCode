417800837e3d14b1a930407e4efcd4d573e0c3f0
inviting <53403310+372216773@users.noreply.github.com>
2021-01-14 18:04:45 +0800

    Java Code

---
+++ 20200114/src/Demo1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/*
+不借用任何字符串库函数实现无冗余的接收两个字符串,
+然后把他们无冗余的连接起来
+输入描述:
+每一行包含两个字符串,长度不超过100
+示例:
+输入:
+abc def
+输出:
+abcdef
+ */
+public class Demo1 {
+
+    public static String func(String str) {
+        String ret="";
+        String[] string=mySplit(str);
+        for (String ss:string) {
+            ret+=ss;
+        }
+        return ret;
+    }
+
+    public  static String[] mySplit(String string) {
+        char[] chars=string.toCharArray();
+        String[] strings=new String[chars.length];
+
+            for (String str:strings) {
+                for (char ch:chars) {
+                    if (ch!=' ') {
+                        str+=ch;
+                    }
+                }
+        }
+            return strings;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        //nextLine()可以接受空格
+        System.out.println(func(scanner.nextLine()));
+    }
+}
