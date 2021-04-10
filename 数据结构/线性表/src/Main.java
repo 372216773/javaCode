de13dc2564acea072925b798fe22b26db1df3ebe
inviting <53403310+372216773@users.noreply.github.com>
2021-04-10 13:11:17 +0800

    mybatis基础操作 1.过时操作 2.流行的-->利用动态代理进行的操作 3.动态sql

--- 数据结构/线性表/src/Main.java
+++ 数据结构/线性表/src/Main.java
@@ -1,50 +1,17 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-        Node head = null;
-        Node tail = null;
-        for (int i = 0; i < n; i++) {
-            int data = scanner.nextInt();
-            Node newNode = new Node(data);
-            if (head == null) {
-                head = tail = newNode;
-            } else {
-                tail = tail.next = newNode;
-            }
+        String str = scanner.next();
+        int length = str.length();
+        int num = Integer.parseInt(str);
+        int result = 0;
+        while ((length--)>0) {
+            int num1 = ((num / (int) Math.pow(10, length)) % 2 == 0) ? 0 : 1;
+            result = result * 10 + num1;
+            num = num %(int) Math.pow(10,length);
         }
-        int delete = scanner.nextInt();
-        Node nodeP = head;
-        Node nodeBefore = null;
-        while (nodeP != null) {
-            if (nodeP.data == delete) {
-                if (nodeP == head) {
-                    nodeP = head = head.next;
-                } else {
-                    nodeBefore.next = nodeP.next;
-                    nodeP = nodeP.next;
-                }
-                n--;
-            } else {
-                nodeBefore = nodeP;
-                nodeP=nodeP.next;
-            }
-        }
-        System.out.println(n);
-        for (Node node = head; node != null; node = node.next) {
-            System.out.print(node.data + " ");
-
-        }
-    }
-}
-
-class Node {
-    public int data;
-    public Node next;
-
-    public Node(int data) {
-        this.data = data;
+        System.out.println(result);
     }
 }
